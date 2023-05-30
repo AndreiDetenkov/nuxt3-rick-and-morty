eb954203b417ca5f1f5bf287456a9f79a5bbd90d
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-05-30 21:15:33 +0600

    chore: install nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,24 @@ import eslintPlugin from 'vite-plugin-eslint'
 import svgLoader from 'vite-svg-loader'
 
 export default defineNuxtConfig({
-  modules: ['@vueuse/nuxt'],
+  modules: ['@vueuse/nuxt', '@nuxt/devtools'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [eslintPlugin(), svgLoader()],
   },
+
   typescript: { strict: true },
+
+  devtools: {
+    enabled: true,
+  },
 })
