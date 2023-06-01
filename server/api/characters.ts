7fc1ff4aca4a4418cb18e48cd9f161948ebec046
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-06-01 16:33:52 +0600

    feat: get characters from server

---
+++ server/api/characters.ts
@@ -0,0 +1,25 @@
+import { Character } from '~/server/api/types'
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function generateRandomNumbers(): number[] {
+  const arr = []
+  for (let i = 0; i < 10; i++) {
+    const n = getRandomInt(1, 826)
+    arr.push(n)
+  }
+  return arr
+}
+
+export default defineEventHandler(async () => {
+  const ids: number[] = generateRandomNumbers()
+
+  const response: Character[] = await $fetch(
+    `https://rickandmortyapi.com/api/character/${ids}`
+  )
+  return response
+})
