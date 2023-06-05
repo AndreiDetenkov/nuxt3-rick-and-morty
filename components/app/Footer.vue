2a65e1ed261e2adf385654a3a162eda4ee10fbfe
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-06-05 13:22:57 +0600

    feat: update styling

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -2,7 +2,7 @@
 
 <template>
   <footer
-    class="flex min-h-[calc(120px)] w-full items-center justify-center bg-neutral-800 px-6 text-neutral-400"
+    class="flex min-h-[calc(120px)] w-full items-center justify-center border-t-[1px] border-neutral-200 px-6 text-neutral-400 dark:border-neutral-600"
   >
     The Rick and Morty
   </footer>
--- components/section/Hero.vue
+++ components/section/Hero.vue
@@ -4,7 +4,7 @@ import Hero from 'assets/icons/hero.svg?skipsvgo'
 
 <template>
   <section
-    class="relative flex h-[calc(50vh_-_60px)] flex-col items-center justify-center text-center"
+    class="relative flex h-[calc(50vh_-_60px)] flex-col items-center justify-center border-b-[1px] border-neutral-200 text-center dark:border-neutral-600"
   >
     <h1
       class="z-10 text-5xl font-black text-dark-gray dark:text-gray-lighten sm:text-6xl md:text-7xl lg:text-8xl"
