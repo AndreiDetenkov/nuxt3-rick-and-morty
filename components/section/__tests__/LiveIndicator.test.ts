da5bddc4dd3b3bf588dbc7137ccba2a3c5db6eac
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-07-10 08:40:27 +0600

    feat: add test for live indicator component

--- components/section/__tests__/LiveIndicator.test.ts
+++ components/section/__tests__/LiveIndicator.test.ts
@@ -1,13 +1,25 @@
-import { describe, it } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
 import LiveIndicator from '../LiveIndicator.vue'
+import { StatusEnum } from '~/utils/types'
 
-const factory = (args: any) => {
-  return mount(LiveIndicator, { ...args })
+const factory = (args?: any) => {
+  return shallowMount(LiveIndicator, { ...args })
 }
 
 describe('LiveIndicator', () => {
-  it('should render live indicator', () => {
-    const wrapper = factory()
+  it('should render live indicator with default color', () => {
+    const wrapper = factory({ props: { status: '' } })
+    expect(wrapper.classes()).toContain('bg-neutral-400')
+  })
+
+  it('should render live indicator with dead color', () => {
+    const wrapper = factory({ props: { status: StatusEnum.Dead } })
+    expect(wrapper.classes()).toContain('bg-red-500')
+  })
+
+  it('should render live indicator with live color', () => {
+    const wrapper = factory({ props: { status: StatusEnum.Alive } })
+    expect(wrapper.classes()).toContain('bg-green-500')
   })
 })
