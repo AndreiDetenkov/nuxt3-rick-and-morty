c1ba7eae5f2804319df4d2f207384945abf23cc2
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-12 18:27:31 +0600

    feat: add default layout and index page

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheHeader from "~/components/the-header.vue";
+import TheFooter from "~/components/the-footer.vue";
+</script>
+
+<template>
+  <NuxtLoadingIndicator/>
+  <the-header/>
+  <slot/>
+  <the-footer/>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1>page works!</h1>
+</template>
