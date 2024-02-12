ccac82776fcf258a016c61ec2e53a60dd616d897
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-12 19:44:35 +0600

    chore: add nuxt color-mode lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.scss"],
+
   typescript: {
-    typeCheck: true,
+    typeCheck: false,
     strict: true
   },
 
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classSuffix: ''
+  },
+
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css'
+  },
+
+
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  darkMode: 'class',
+  content: [],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        title: '#202329',
+        primary: '#3C3E44',
+        'primary-light': '#9e9e9e',
+        'primary-lighter': '#f5f5f5',
+        'primary-dark': '#333',
+        'primary-darker': '#272B33',
+      },
+    },
+  },
+  plugins: [],
+}
+
