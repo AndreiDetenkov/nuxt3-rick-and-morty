c18db9fd1332e10510721d99854ea0f71ca2caa4
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-19 09:32:41 +0600

    style: styling toggle btn

--- components/ToggleColorMode/ToggleColorMode.client.vue
+++ components/ToggleColorMode/ToggleColorMode.client.vue
@@ -9,8 +9,12 @@ defineEmits<{
 </script>
 
 <template>
-  <button data-test="toggle-button" @click="$emit('toggleColorMode', $colorMode.preference)">
-    <svgo-sun v-if="isDarkMode" data-test="sun" class="h-5 w-5 text-primary-light" />
-    <svgo-moon v-else data-test="moon" class="h-5 w-5" />
+  <button
+    data-test="toggle-button"
+    class="border rounded-lg hover:border-primary dark:border-primary dark:hover:border-primary-light transition duration-300 flex items-center justify-center p-2"
+    @click="$emit('toggleColorMode', $colorMode.preference)"
+  >
+    <svgo-sun v-if="isDarkMode" data-test="sun" class="h-4 w-4" />
+    <svgo-moon v-else data-test="moon" class="h-4 w-4" />
   </button>
 </template>
