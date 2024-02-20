6699a5d4acc8b57cb86a48660bf2de9ecf98cb94
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-20 22:20:21 +0600

    style: styling of character card

--- components/Card/CharacterCard.vue
+++ components/Card/CharacterCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import type { Character } from '~/stores/types'
+import LiveIndicator from '~/components/LiveIndicator.vue'
 
 const props = defineProps<{ character: Character }>()
 
@@ -11,48 +12,52 @@ const episode = computed(() => {
 
 <template>
   <article
-    class="m-3 flex h-auto w-full max-w-[600px] flex-col overflow-hidden rounded-lg bg-gray shadow-md shadow-dark-gray-darker md:h-[220px] md:flex-row"
+    class="m-3 flex h-auto max-w-[600px] flex-col overflow-hidden rounded-lg bg-primary shadow-md shadow-dark-gray-darker md:h-[220px] md:flex-row"
   >
-    <div class="w-full md:w-2/5">
+    <div class="md:w-2/5">
       <nuxt-img
         :src="character.image"
         :alt="character.name"
-        class="h-[300px] w-full object-cover md:h-full"
+        class="h-[240px] w-full object-contain mt-4 sm:mt-0 sm:object-cover md:h-full"
         loading="lazy"
       />
     </div>
 
-    <div class="flex w-full flex-col p-3 text-white md:w-3/5">
+    <div class="flex flex-col items-center sm:items-start text-center sm:text-left p-3 lg:pl-6 text-white md:w-3/5">
       <section class="flex flex-1 flex-col justify-start">
-        <nuxt-link :to="`/character/${character.id}`">
+        <NuxtLink :to="`/character/${character.id}`">
           <h2
             class="truncate text-2xl font-bold tracking-wide text-light-gray transition-colors duration-300 hover:text-orange-400"
           >
             {{ character.name }}
           </h2>
-        </nuxt-link>
-        <span class="flex items-center text-sm capitalize">
-          <!--          <LiveIndicator :status="character.status" /> -->
+        </NuxtLink>
+        <div class="flex items-center text capitalize">
+          <LiveIndicator :status="character.status" />
           {{ character.status }} - {{ character.species }}
-        </span>
+        </div>
       </section>
 
       <section class="mt-5 flex flex-1 flex-col justify-center md:mt-0">
-        <span class="text-sm text-gray-lighten"> Last known location: </span>
-        <nuxt-link to="/">
+        <p class="text text-primary-light mb-1">
+          Last known location:
+        </p>
+        <NuxtLink to="/">
           <h2 class="text-md text-light-gray transition-colors duration-300 hover:text-orange-400">
             {{ character.location.name }}
           </h2>
-        </nuxt-link>
+        </NuxtLink>
       </section>
 
       <section class="mt-5 flex flex-1 flex-col justify-end md:mt-0">
-        <span class="text-sm text-gray-lighten"> First seen in: </span>
-        <nuxt-link to="/">
+        <p class="text text-primary-light mb-1">
+          First seen in:
+        </p>
+        <NuxtLink to="/">
           <h2 class="text-md text-light-gray transition-colors duration-300 hover:text-orange-400">
             episode: {{ episode }}
           </h2>
-        </nuxt-link>
+        </NuxtLink>
       </section>
     </div>
   </article>
