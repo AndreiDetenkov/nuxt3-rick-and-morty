70a093a467578d71f7743355645000c52fbd736a
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-20 21:22:33 +0600

    reactor: implement hero section

---
+++ components/HeroSection.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
+
+<template>
+  <div class="relative grid place-items-center h-full">
+    <h1
+      class="z-10 text-center text-5xl font-black text-title dark:text-primary-light sm:text-6xl md:text-7xl lg:text-8xl"
+    >
+      The Rick and Morty
+    </h1>
+
+    <div class="absolute bottom-0 left-0 right-0 h-full w-full p-4">
+      <svgo-hero
+        :font-controlled="false"
+        class="h-full w-full fill-primary-lighter dark:fill-primary-light/20"
+      />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,22 @@
 <script setup lang="ts">
 import { useCharactersStore } from '~/stores/characters'
 import CharacterCard from '~/components/Card/CharacterCard.vue'
+import HeroSection from '~/components/HeroSection.vue'
 
 const store = useCharactersStore()
 store.getCharactersByIds()
 </script>
 
 <template>
-  <section id="hero" class="h-[calc(50vh)] py-4">
-    <div class="relative grid place-items-center h-full">
-      <h1
-        class="z-10 text-center text-5xl font-black text-title dark:text-primary-light sm:text-6xl md:text-7xl lg:text-8xl"
-      >
-        The Rick and Morty
-      </h1>
-      <div class="absolute bottom-0 left-0 right-0 h-full w-full p-4">
-        <svgo-hero :font-controlled="false" class="h-full w-full fill-primary-lighter dark:fill-primary-light/20" />
-      </div>
-    </div>
+  <section class="h-[calc(40vh)] py-4">
+    <HeroSection />
   </section>
 
-  <section class="grid sm:grid-cols-2 2xl:grid-cols-3 gap-8">
-    <CharacterCard v-for="character in store.characters" :key="character.id.toString()" :character="character" />
+  <section class="grid sm:grid-cols-2 gap-8">
+    <CharacterCard
+      v-for="character in store.characters"
+      :key="character.id.toString()"
+      :character="character"
+    />
   </section>
 </template>
