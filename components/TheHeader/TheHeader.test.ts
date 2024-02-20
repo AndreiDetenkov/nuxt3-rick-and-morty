b5c8172a17cba713d02158b1eb9dcc237420c74a
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-20 22:25:05 +0600

    test: refactor the header test

--- components/TheHeader/TheHeader.test.ts
+++ components/TheHeader/TheHeader.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, it } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import TheHeader from '~/components/TheHeader/TheHeader.vue'
 
 describe('theHeader components', () => {
   it('should render the header', () => {
-    const wrapper = shallowMount(TheHeader)
+    const wrapper = mount(TheHeader)
 
     expect(wrapper.find('header').exists()).toBe(true)
   })
 
   it('should display named slots it the are exists', () => {
-    const wrapper = shallowMount(TheHeader, {
+    const wrapper = mount(TheHeader, {
       slots: {
         logo: '<div>Logo slot</div>',
         nav: '<div>Nav slot</div>',
