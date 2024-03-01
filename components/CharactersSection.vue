b20662b07fec931310b018cce414da67ae1821f1
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-02 00:12:47 +0600

    refactor: reorganize structure

--- components/CharactersSection.vue
+++ components/CharactersSection.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import CharacterCard from '~/components/CharacterCard.vue'
-import TheContainer from '~/components/TheContainer.vue'
-import { useCharactersStore } from '~/stores/characters.js'
-
-const store = useCharactersStore()
-store.getRandomCharacters()
 </script>
 
 <template>
   <TheContainer>
     <div class="grid sm:grid-cols-2 gap-8">
-      <CharacterCard
-        v-for="character in store.characters"
-        :key="character.id.toString()"
-        :character="character"
-      />
+      <slot />
     </div>
   </TheContainer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { useSeoMeta } from '#imports'
-
 import HeroSection from '~/components/HeroSection.vue'
 import CharactersSection from '~/components/CharactersSection.vue'
+import CharacterCard from '~/components/CharacterCard.vue'
 
 useSeoMeta({
   title: 'Rick and Morty - Main Page',
@@ -11,6 +10,9 @@ useSeoMeta({
   ogDescription: 'Rick and Morty Api random characters',
   ogImage: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
 })
+
+const store = useCharactersStore()
+store.getRandomCharacters()
 </script>
 
 <template>
@@ -19,6 +21,12 @@ useSeoMeta({
   </section>
 
   <section class="py-20 border-b dark:border-b-primary">
-    <CharactersSection />
+    <CharactersSection>
+      <CharacterCard
+        v-for="character in store.characters"
+        :key="character.id.toString()"
+        :character="character"
+      />
+    </CharactersSection>
   </section>
 </template>
