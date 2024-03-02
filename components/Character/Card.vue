a95fcb2ed811a5a1277ea498a83b990ca67ab05e
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-02 16:30:33 +0600

    style: update styles

--- components/Character/Card.vue
+++ components/Character/Card.vue
@@ -23,7 +23,7 @@ const episode = computed(() => {
     </div>
 
     <div class="flex flex-col items-center sm:items-start text-center sm:text-left p-3 lg:pl-6 text-white md:w-3/5">
-      <section class="flex flex-1 flex-col justify-start">
+      <section class="w-full flex flex-1 flex-col justify-start">
         <NuxtLink :to="`/character/${character.id}`">
           <h2
             class="truncate text-2xl font-bold tracking-wide text-light-gray transition-colors duration-300 hover:text-orange-400"
@@ -31,7 +31,7 @@ const episode = computed(() => {
             {{ character.name }}
           </h2>
         </NuxtLink>
-        <div class="flex items-center text capitalize">
+        <div class="flex justify-center sm:justify-start items-center text capitalize">
           <LiveIndicator :status="character.status" />
           {{ character.status }} - {{ character.species }}
         </div>
