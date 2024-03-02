f89e38b699cd8023ba2ffab6b774a68c80c4943e
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-02 16:20:54 +0600

    style: update icons styling

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,17 +3,17 @@
 </script>
 
 <template>
-  <footer class="flex items-center justify-center px-4 py-10 space-x-4">
+  <footer class="flex items-center justify-center px-4 py-10 space-x-6">
     <NuxtLink to="https://github.com/afuh/rick-and-morty-api" target="_blank">
-      <svgo-github class="w-8 h-8 fill-primary-light hover:fill-orange-400 transition duration-300" />
+      <svgo-github class="w-6 h-6 fill-primary-light hover:fill-orange-400 transition duration-300" />
     </NuxtLink>
 
     <NuxtLink to="https://twitter.com/rickandmortyapi" target="_blank">
-      <svgo-twitter class="w-8 h-8 fill-primary-light hover:fill-orange-400 transition duration-300" />
+      <svgo-twitter class="w-6 h-6 fill-primary-light hover:fill-orange-400 transition duration-300" />
     </NuxtLink>
 
     <NuxtLink to="https://rickandmortyapi.com/support-us" target="_blank">
-      <svgo-monster class="w-8 h-8 fill-primary-light hover:fill-orange-400 transition duration-300" />
+      <svgo-monster class="w-6 h-6 fill-primary-light hover:fill-orange-400 transition duration-300" />
     </NuxtLink>
   </footer>
 </template>
