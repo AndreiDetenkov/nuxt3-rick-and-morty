6a08c2d6d04710ca788ebd71b5b46befb9820af6
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-02 16:05:13 +0600

    feat: add link icons into footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
-  <footer>footer</footer>
+  <footer class="flex items-center justify-center px-4 py-10 space-x-4">
+    <NuxtLink to="https://github.com/afuh/rick-and-morty-api" target="_blank">
+      <svgo-github class="w-8 h-8 fill-primary-light hover:fill-orange-400 transition duration-300" />
+    </NuxtLink>
+
+    <NuxtLink to="https://twitter.com/rickandmortyapi" target="_blank">
+      <svgo-twitter class="w-8 h-8 fill-primary-light hover:fill-orange-400 transition duration-300" />
+    </NuxtLink>
+
+    <NuxtLink to="https://rickandmortyapi.com/support-us" target="_blank">
+      <svgo-monster class="w-8 h-8 fill-primary-light hover:fill-orange-400 transition duration-300" />
+    </NuxtLink>
+  </footer>
 </template>
