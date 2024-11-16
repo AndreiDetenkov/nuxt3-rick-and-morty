259b99df7c9f893b47ac34369203e365351070d0
andrei.detenkov <a.detenkov@gmail.com>
2024-11-16 22:07:40 +0600

    refactor: update characters grid / container

--- app/components/section/Characters.vue
+++ app/components/section/Characters.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="py-20 border-b dark:border-b-primary border-t dark:border-t-primary">
     <base-container>
-      <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-4 xl:gap-6">
+      <div class="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
         <slot />
       </div>
     </base-container>
