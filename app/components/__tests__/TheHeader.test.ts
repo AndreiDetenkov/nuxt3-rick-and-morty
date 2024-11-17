c13ee82acda21a8fd7e3dd85afea9f90629b1319
andrei.detenkov <a.detenkov@gmail.com>
2024-11-17 21:23:02 +0600

    test: improve test for Header component

--- app/components/__tests__/TheHeader.test.ts
+++ app/components/__tests__/TheHeader.test.ts
@@ -6,18 +6,16 @@ describe('theHeader components', () => {
   it('should render the header', () => {
     const wrapper = mount(TheHeader)
 
-    expect(wrapper.find('header').exists()).toBe(true)
+    expect(wrapper.find('[data-test="header"]').exists()).toBe(true)
   })
 
   it('should display named slots it the are exists', () => {
     const wrapper = mount(TheHeader, {
       slots: {
         logo: '<div>Logo slot</div>',
-        nav: '<div>Nav slot</div>',
       },
     })
 
     expect(wrapper.html()).toContain('<div>Logo slot</div>')
-    expect(wrapper.html()).toContain('<div>Nav slot</div>')
   })
 })
--- app/components/base/Header.vue
+++ app/components/base/Header.vue
@@ -3,7 +3,7 @@
 
 <template>
   <base-container>
-    <div class="relative min-h-[60px] w-full flex items-center py-4">
+    <div class="relative min-h-[60px] w-full flex items-center py-4" data-test="header">
       <slot name="logo" />
       <slot />
     </div>
