d0bfeee3ba32e28fd7b586ba4c2fddc30f455ec0
andrei.detenkov <a.detenkov@gmail.com>
2024-11-23 23:23:14 +0600

    chore: update components naming in template

--- app/pages/character/[id].vue
+++ app/pages/character/[id].vue
@@ -14,13 +14,13 @@ store.getCharacterById(Number(route.params.id))
 </script>
 
 <template>
-  <Loading v-if="store.isLoading" />
-  <TheContainer v-else>
+  <loading v-if="store.isLoading" />
+  <base-container v-else>
     <section
       class="mx-auto mt-7 rounded-xl border p-7 w-full md:w-2/3 lg:w-2/4 xl:w-2/5 2xl:w-1/3 relative"
     >
-      <CloseButton class="absolute -top-4 -right-4" @on-click="$router.back()" />
-      <CharacterInfo :character="store.character" />
+      <close-button class="absolute -top-4 -right-4" @on-click="$router.back()" />
+      <character-info :character="store.character" />
     </section>
-  </TheContainer>
+  </base-container>
 </template>
