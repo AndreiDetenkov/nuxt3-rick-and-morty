95988ef5fa5d94be110df3645def9982756a7e7b
andrei.detenkov <a.detenkov@gmail.com>
2025-01-04 23:31:53 +0600

    chore: add rule for vue html warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,16 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt({
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+      },
+    ],
+  },
+})
